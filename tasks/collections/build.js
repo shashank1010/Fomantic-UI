cdc6909ee9f7bb2042d01edf56dc87a8152ebe1f
Shashank Agarwal <shashank1010@gmail.com>
2020-01-11 01:42:05 +0530

    Added semantic ui styling with integrations

--- tasks/collections/build.js
+++ tasks/collections/build.js
@@ -9,6 +9,7 @@ module.exports = function (gulp) {
     watch       = require('./../watch'),
 
     build       = require('./../build'),
+    buildCustom     = require('./../build/custom'),
     buildJS     = require('./../build/javascript'),
     buildCSS    = require('./../build/css'),
     buildAssets = require('./../build/assets')
@@ -19,6 +20,9 @@ module.exports = function (gulp) {
 
   gulp.task('build', build);
   gulp.task('build').description = 'Builds all files from source';
+
+  gulp.task('build-custom', buildCustom);
+  gulp.task('build-custom').description = 'Builds assets and css files from source';
 
   gulp.task('build-javascript', buildJS);
   gulp.task('build-javascript').description = 'Builds all javascript from source';
